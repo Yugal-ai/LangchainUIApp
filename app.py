cb51f74f342dd1716f910074eb52f9312fd5758b
Yugal Warade <yugalwarade@gmail.com>
2023-05-18 17:09:45 +0530

    Made few changes

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from langchain.vectorstores import  FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
+import streamlit_authenticator as stauth
 
 def main():
     """ This is main funtion of this program, All execution will work from here """
